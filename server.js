ca3115545b00fcb919e945c81fd3edbc97c81cf1
Billy Richardson <billy7373731@outlook.com>
2023-05-09 14:39:00 +0100

    c2

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const app = require("./src/app")
 const db = require("./db/connection");
-const Restaurant = require("./models");
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
-app.get("./App", async(req,res)=> {
-    const rest = Restaurant.findAll()
-    
-    res.json(rest)
-})
 
-app.listen(port, () => {
-    db.sync();
+app.listen(port, async() => {
+    await db.sync();
     console.log(`Listening at http://localhost:${port}/restaurants`);
 })
--- src/app.js
+++ src/app.js
@@ -5,6 +5,17 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
+app.set("json spaces", 2)
+app.get("/restaurants", async(req,res)=> {
+    try{const rest = await Restaurant.findAll()
+    
+    res.json(rest)
+}
+    catch(error){
+        next(error)
+    }
+    
+})
 
 
 
