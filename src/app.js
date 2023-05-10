9dcfb2c16aa384815a2e7ea2eca9f2b1ab165a7b
Billy Richardson <billy7373731@outlook.com>
2023-05-10 12:02:36 +0100

    commit use

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const db = require("../db/connection");
 //TODO: Create your GET Request Route Below: 
 
 app.set("json spaces", 2)
-app.get("/restaurants", async(req,res)=> {
+app.get("/restaurants", async(req,res,next)=> {
     try{const rest = await Restaurant.findAll()
     
     res.json(rest)
@@ -14,7 +14,13 @@ app.get("/restaurants", async(req,res)=> {
     catch(error){
         next(error)
     }
-    
+})
+
+app.post("");
+
+app.use((err,req,res,next)=>{
+    console.log.error(err.stack);
+    res.status(500).send("Broken")
 })
 
 
